9c99354ada5e550a404c807becfbb3bb717440bf
vitalii_eremin <VTB4048139@region.vtb.ru>
2021-09-23 20:05:37 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/Main.java
@@ -0,0 +1,10 @@
+package com.geekbrains;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        for(int i=0;i<a; i++) {
+            System.out.println(a);
+        }
+    }
+}
